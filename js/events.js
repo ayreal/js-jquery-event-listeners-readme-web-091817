1ab9c26fa4c59a7d4e0a6d0d4abc6106c7d55508
Ariel Herman <aeh.herman@gmail.com>
2017-11-02 22:14:11 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
 
-// call functions here
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  });
+}
 
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(key) {
+    if (key.which == 71) {
+      alert("g was pressed");
+    }
+  });
+}
+
+$(document).ready(function() {
+  // call functions here
 });
+
+getIt();
+frameIt();
+submitIt();
+pressIt();
